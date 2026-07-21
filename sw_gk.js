f895f56b04dd820f00532b907e8dcff52610f37b
performanceprojectpp-sudo <performanceprojectpp@gmail.com>
2026-07-21 12:41:30 +0200

    Add files via upload

---
+++ sw_gk.js
@@ -0,0 +1,33 @@
+const CACHE = 'gk-mosy-v1';
+const ASSETS = [
+  './',
+  './gk_mosy.html',
+  './gk_manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './logo1.jpg',
+  './logo2.jpg',
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        if(response && response.status === 200 && response.type === 'basic'){
+          const clone = response.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => caches.match('./gk_mosy.html'));
+    })
+  );
+});
